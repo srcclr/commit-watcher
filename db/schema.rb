84f51723f4f9e87d7a4df9b0de7b9f150096cb0d
Caleb Fenton <calebjfenton@gmail.com>
2016-02-24 16:51:39 -0800

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -2,35 +2,46 @@ Sequel.migration do
   change do
     create_table(:configurations) do
       primary_key :id, :type=>"int(11) unsigned"
-      column :crawl_frequency, "int(11) unsigned", :default=>1440, :null=>false
-      column :global_patterns, "text", :null=>false
+      column :audit_frequency, "int(11) unsigned", :default=>1440, :null=>false
+      column :global_rules, "text", :null=>false
       column :github_token, "char(40)", :default=>"", :null=>false
-    end
-    
-    create_table(:patterns) do
-      primary_key :id, :type=>"int(11) unsigned"
-      column :pattern, "text", :null=>false
     end
     
     create_table(:projects) do
       primary_key :id, :type=>"int(11) unsigned"
       column :name, "varchar(100)", :default=>"", :null=>false
-      column :patterns, "text"
-      column :ignore_global_patterns, "tinyint(1) unsigned", :default=>false, :null=>false
-      column :next_crawl, "int(11) unsigned", :default=>0, :null=>false
-      column :last_commit, "char(40)"
+      column :rules, "text"
+      column :ignore_global_rules, "tinyint(1) unsigned", :default=>false, :null=>false
+      column :next_audit, "int(11) unsigned", :default=>0, :null=>false
       column :date_created, "timestamp", :default=>Sequel::CURRENT_TIMESTAMP, :null=>false
+      column :last_commit_time, "timestamp", :null=>true
+      
+      index [:name], :name=>:name, :unique=>true
+    end
+    
+    create_table(:rule_types) do
+      primary_key :id, :type=>"int(11) unsigned"
+      column :name, "varchar(30)", :default=>"", :null=>false
     end
     
     create_table(:commits) do
       primary_key :id, :type=>"int(11) unsigned"
       foreign_key :project_id, :projects, :type=>"int(11) unsigned", :null=>false, :key=>[:id]
       column :commit_date, "datetime", :null=>false
-      column :matched_patterns, "mediumtext", :null=>false
-      column :commit, "char(40)", :default=>"", :null=>false
+      column :commit_hash, "char(40)", :null=>false
+      column :audit_results, "text", :null=>false
       column :date_created, "timestamp", :default=>Sequel::CURRENT_TIMESTAMP, :null=>false
       
       index [:project_id], :name=>:project_id
+      index [:commit_hash], :name=>:uq_commit_hash, :unique=>true
+    end
+    
+    create_table(:rules) do
+      primary_key :id, :type=>"int(11) unsigned"
+      foreign_key :rule_type_id, :rule_types, :type=>"int(11) unsigned", :null=>false, :key=>[:id]
+      column :rule, "text", :null=>false
+      
+      index [:rule_type_id], :name=>:fk_rule_type_id
     end
   end
 end
