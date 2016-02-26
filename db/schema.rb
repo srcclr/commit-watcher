81adbe92961f2c9cb474a34b175074385e9f3873
Caleb Fenton <calebjfenton@gmail.com>
2016-02-26 13:47:46 -0800

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@ Sequel.migration do
     create_table(:projects) do
       primary_key :id, :type=>"int(11) unsigned"
       column :name, "varchar(100)", :default=>"", :null=>false
-      column :rules, "text"
+      column :rules, "text", :null=>false
       column :ignore_global_rules, "tinyint(1) unsigned", :default=>false, :null=>false
       column :next_audit, "int(11) unsigned", :default=>0, :null=>false
-      column :date_created, "timestamp", :default=>Sequel::CURRENT_TIMESTAMP, :null=>false
       column :last_commit_time, "timestamp", :null=>true
+      column :date_created, "timestamp", :default=>Sequel::CURRENT_TIMESTAMP, :null=>false
       
       index [:name], :name=>:name, :unique=>true
     end
@@ -29,7 +29,7 @@ Sequel.migration do
       foreign_key :project_id, :projects, :type=>"int(11) unsigned", :null=>false, :key=>[:id]
       column :commit_date, "datetime", :null=>false
       column :commit_hash, "char(40)", :null=>false
-      column :audit_results, "text", :null=>false
+      column :audit_results, "longtext", :null=>false
       column :date_created, "timestamp", :default=>Sequel::CURRENT_TIMESTAMP, :null=>false
       
       index [:project_id], :name=>:project_id
@@ -37,7 +37,7 @@ Sequel.migration do
     end
     
     create_table(:rules) do
-      primary_key :id, :type=>"int(11) unsigned"
+      primary_key :id, :type=>"int(10) unsigned"
       foreign_key :rule_type_id, :rule_types, :type=>"int(11) unsigned", :null=>false, :key=>[:id]
       column :rule, "text", :null=>false
       
