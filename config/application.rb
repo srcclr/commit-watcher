a35d16e3f4e29f2d833141fe5de678828e1294e9
Caleb Fenton <calebjfenton@gmail.com>
2016-03-09 14:22:24 -0800

    Fix production initialization

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,6 @@ module CommitWatcher
 
     Sequel::Database.extension :pagination
 
-    config.web_console.development_only = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
