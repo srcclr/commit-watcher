b8914920a1a0f90ca951b69da2d6b7439ac18cc3
Caleb Fenton <calebjfenton@gmail.com>
2016-04-05 10:53:02 -0700

    Add link to GH commit

--- app/controllers/commits_controller.rb
+++ app/controllers/commits_controller.rb
@@ -1,9 +1,16 @@
 class CommitsController < ApplicationController
   def index
-    @commits = Commits
+    @commits = Commits.join(:projects, id: :project_id)
+        .select_all(:commits)
+        .select_append(:projects__name)
   end
 
   def show
-    @commit = Commits[id: params[:id].to_i]
+    id = params[:id].to_i
+    ds = Commits.join(:projects, id: :project_id)
+        .select_all(:commits)
+        .select_append(:projects__name)
+        .where(commits__id: id)
+    @commit = ds.first
   end
 end
