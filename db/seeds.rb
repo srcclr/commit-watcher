bc34be8b60080b50ce80850ccea328e2752c928a
Caleb Fenton <calebjfenton@gmail.com>
2016-04-05 10:52:09 -0700

    Move noisy rules to medium

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ rules = [
     {
         name: 'strong_vuln_patterns',
         rule_type_id: 4,
-        value: '(?i)(advisory|attack|(un)?authoriz(e|ation)|clickjack|crack|cross.site|csrf|\bCVE\b|deadlock|denial.of.service|\bEOP\b|exploit|hack|hijack|infinite.loop|malicious|\bNVD\b|OSVDB|\bPoC\b|proof.of.concept|\bRCE\b|\bReDoS\b|remote.code.execution|security|victim|\bvuln|\bXEE\b|\bXSRF\b|\bXSS\b|\bXXE\b)',
+        value: '(?i)(advisory|attack|(un)?authoriz(e|ation)|clickjack|cross.site|csrf|\bCVE\b|deadlock|denial.of.service|\bEOP\b|exploit|hijack|infinite.loop|malicious|\bNVD\b|OSVDB|\bRCE\b|\bReDoS\b|remote.code.execution|security|victim|\bvuln|\bXEE\b|\bXSRF\b|\bXSS\b|\bXXE\b)',
         description: 'Strong indication of a security fix'
     },
     {
@@ -22,7 +22,7 @@ rules = [
     {
         name: 'medium_vuln_patterns',
         rule_type_id: 4,
-        value: '(?i)(authenticat(e|ion)|brute force|bypass|constant.time|credential|\bDoS\b|expos(e|ing)|harden|injection|lockout|overflow|password|poison|privelage|\b(in)?secur(e|ity)|(de)?serializ|spoof|timing|traversal)',
+        value: '(?i)(authenticat(e|ion)|brute force|bypass|constant.time|crack|credential|\bDoS\b|expos(e|ing)|hack|harden|injection|lockout|overflow|password|\bPoC\b|proof.of.concept|poison|privelage|\b(in)?secur(e|ity)|(de)?serializ|spoof|timing|traversal)',
         description: 'Moderate indication of a security fix'
     },
     {
