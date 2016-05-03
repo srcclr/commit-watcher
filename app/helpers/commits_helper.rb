c7cc44803d539a19eb313c63b9dbdc3f3717f1f8
Jason Yeo <jasonyeo88@gmail.com>
2016-05-03 22:43:43 +0800

    Allow sorting of columns

--- app/helpers/commits_helper.rb
+++ app/helpers/commits_helper.rb
@@ -15,4 +15,10 @@ limitations under the License.
 =end
 
 module CommitsHelper
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
+    direction = (column == sort_column && sort_direction == 'asc') ? 'desc' : 'asc'
+    link_to title, {:order => column, :direction => direction}, {:class => css_class}
+  end
 end
