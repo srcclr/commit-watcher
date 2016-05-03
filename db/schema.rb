0f36132d9d91395597e279f0a4b9372eb2a6a3c3
Caleb Fenton <calebjfenton@gmail.com>
2016-05-03 15:33:18 -0700

    Update schema with status_type_id column

--- db/schema.rb
+++ db/schema.rb
@@ -1,19 +1,3 @@
-=begin
-Copyright 2016 SourceClear Inc
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-=end
-
 Sequel.migration do
   change do
     create_table(:configurations) do
@@ -58,6 +42,7 @@ Sequel.migration do
     create_table(:commits) do
       primary_key :id, :type=>"int(11) unsigned"
       foreign_key :project_id, :projects, :type=>"int(11) unsigned", :null=>false, :key=>[:id], :on_delete=>:cascade
+      column :status_type_id, "tinyint(3) unsigned", :default=>0, :null=>false
       column :commit_date, "datetime", :null=>false
       column :commit_hash, "char(40)", :null=>false
       column :audit_results, "longtext", :null=>false
