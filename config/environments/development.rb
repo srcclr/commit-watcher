e1c831da19214c492341a03452522708cd27dd31
Caleb Fenton <calebjfenton@gmail.com>
2016-05-16 14:35:53 -0700

    Add email configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,4 +44,14 @@ Rails.application.configure do
 
   # Set log level
   config.log_level = :DEBUG
+
+  # SMTP settings for email notifications
+  config.action_mailer.smtp_settings = {
+    address:               'smtp.gmail.com',
+    port:                  587,
+    user_name:             ENV['GMAIL_USERNAME'],
+    password:              ENV['GMAIL_PASSWORD'],
+    authentication:        'plain',
+    enable_starttls_auto:  true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,16 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   #config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  # SMTP settings for email notifications
+  config.action_mailer.smtp_settings = {
+    address:               'smtp.gmail.com',
+    port:                  587,
+    user_name:             ENV['GMAIL_USERNAME'],
+    password:              ENV['GMAIL_PASSWORD'],
+    authentication:        'plain',
+    enable_starttls_auto:  true
+  }
 end
