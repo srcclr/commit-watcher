53b88d4ba8fdf241002b7c17d32e5ddc94df2aa5
Caleb Fenton <calebjfenton@gmail.com>
2016-05-17 13:37:35 -0700

    Change indentations to 2 spaces

--- spec/app/models/rule_sets_spec.rb
+++ spec/app/models/rule_sets_spec.rb
@@ -1,63 +1,63 @@
 require 'rails_helper'
 
 describe RuleSets do
-    describe '.new' do
-        let(:name) { 'name' }
-        let(:rules) { ['rule1'].to_json }
-        let(:description) { 'test rule set' }
-        let(:rule_set) {
-            r = Rules.new(name: 'rule1', rule_type_id: 1, value: 'val', description: 'desc')
-            r.save
-            RuleSets.new(name: name, rules: rules, description: description)
-        }
+  describe '.new' do
+    let(:name) { 'name' }
+    let(:rules) { ['rule1'].to_json }
+    let(:description) { 'test rule set' }
+    let(:rule_set) {
+      r = Rules.new(name: 'rule1', rule_type_id: 1, value: 'val', description: 'desc')
+      r.save
+      RuleSets.new(name: name, rules: rules, description: description)
+    }
 
-        context 'with valid parameters' do
-            subject { rule_set.valid? }
+    context 'with valid parameters' do
+      subject { rule_set.valid? }
 
-            it { should eq true }
-        end
-
-        context 'with a short name' do
-            let(:name) { 'n' }
-
-            it 'fails with the expected error' do
-                expect(rule_set.valid?).to eq false
-                expect(rule_set.errors).to have_key :name
-            end
-        end
-
-        context 'with an empty rules value' do
-            let(:rules) { [].to_json }
-
-            it 'fails with the expected error' do
-                expect(rule_set.valid?).to eq false
-                expect(rule_set.errors).to have_key :rules
-            end
-        end
-
-        context 'with a rules value that references a non-existant rule' do
-            let(:rules) { ['no-existy'].to_json }
-
-            it 'fails with the expected error' do
-                expect(rule_set.valid?).to eq false
-                expect(rule_set.errors).to have_key :rules
-            end
-        end
-
-        context 'with a rules value thats invalid JSON' do
-            let(:rules) { 'bro, do you even json?' }
-
-            it 'fails with the expected error' do
-                expect(rule_set.valid?).to eq false
-                expect(rule_set.errors).to have_key :rules
-            end
-        end
-
-        context 'with empty description' do
-            let(:description) { '' }
-            subject { rule_set.valid? }
-
-            it { should eq true }
-        end
+      it { should eq true }
     end
+
+    context 'with a short name' do
+      let(:name) { 'n' }
+
+      it 'fails with the expected error' do
+        expect(rule_set.valid?).to eq false
+        expect(rule_set.errors).to have_key :name
+      end
+    end
+
+    context 'with an empty rules value' do
+      let(:rules) { [].to_json }
+
+      it 'fails with the expected error' do
+        expect(rule_set.valid?).to eq false
+        expect(rule_set.errors).to have_key :rules
+      end
+    end
+
+    context 'with a rules value that references a non-existant rule' do
+      let(:rules) { ['no-existy'].to_json }
+
+      it 'fails with the expected error' do
+        expect(rule_set.valid?).to eq false
+        expect(rule_set.errors).to have_key :rules
+      end
+    end
+
+    context 'with a rules value thats invalid JSON' do
+      let(:rules) { 'bro, do you even json?' }
+
+      it 'fails with the expected error' do
+        expect(rule_set.valid?).to eq false
+        expect(rule_set.errors).to have_key :rules
+      end
+    end
+
+    context 'with empty description' do
+      let(:description) { '' }
+      subject { rule_set.valid? }
+
+      it { should eq true }
+    end
+  end
 end
