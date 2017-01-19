81108acd1ffeb041569fa649a13b2e72bd6c9a52
Ang Ming Yi <bluedabedi@hotmail.com>
2017-01-19 09:22:49 +0800

    Only Slack Notify in english

--- lib/slack_notifier.rb
+++ lib/slack_notifier.rb
@@ -5,12 +5,19 @@ class SlackNotifier
     project = Projects[id: project_id]
     @project_name = project.name
     @commit_hash = commit_hash
-    @audit_result = JSON.pretty_generate(audit_result) || {}
+    @audit_result = audit_result
     @webhook = webhook
   end
 
   def notify
     notifier = Slack::Notifier.new @webhook
-    notifier.ping "Commit hash [#{@commit_hash}](https://#{@project_name}/commit/#{commit_hash}) of the project #{@project_name} has been detected to violate the following:\n#{@audit_result}\n"
+    notifier.ping "Commit hash [#{@commit_hash}](https://#{@project_name}/commit/#{commit_hash}) of the project #{@project_name} has been detected to violate the following:\n#{rule_name}\n"
+  end
+
+  private
+
+  def rule_name
+    return @audit_result['rule_name'] if @audit_result.key?('rule_name')
+    'Unknown rule/Improper audit result object returned.'
   end
 end
