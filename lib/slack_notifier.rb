1d73ed73b940caeede909155240150fdda426eed
Ang Ming Yi <bluedabedi@hotmail.com>
2017-01-19 08:31:37 +0800

    Update SlackNotifier payload formatting

--- lib/slack_notifier.rb
+++ lib/slack_notifier.rb
@@ -1,14 +1,16 @@
+require 'json'
+
 class SlackNotifier
   def initialize(webhook, project_id, commit_hash, audit_result)
     project = Projects[id: project_id]
     @project_name = project.name
     @commit_hash = commit_hash
-    @audit_result = audit_result
+    @audit_result = JSON.pretty_generate(audit_result) || {}
     @webhook = webhook
   end
 
   def notify
     notifier = Slack::Notifier.new @webhook
-    notifier.ping "Commit hash #{@commit_hash} of the project #{@project_name} has been detected to violate #{@audit_result}."
+    notifier.ping "Commit hash [#{@commit_hash}](https://#{@project_name}/commit/#{commit_hash}) of the project #{@project_name} has been detected to violate the following:\n#{@audit_result}\n"
   end
 end
